078b454ddf096a6996735ccf018d99968e5aadcd
CharlieMallon <penguin47@hotmail.co.uk>
2021-07-26 17:10:46 +0100

    added frame for add task modal #2 Clicking gray space doesnt close modal

--- static/script.js
+++ static/script.js
@@ -10,3 +10,21 @@ function burgerMenu() {
     var element = document.getElementById("burger-menu");
     element.classList.toggle("burger");
 }
+
+// event listeners for show/hide modal
+
+const button = document.getElementById('add-task');
+const span = document.getElementsByClassName("close")[0];
+const close = document.getElementsByClassName("modal");
+
+button.addEventListener('click', addModal)
+span.addEventListener('click', addModal)
+close.addEventListener('click', addModal)
+
+
+// open/close the modal
+
+function addModal() {
+    var element = document.getElementById("add-modal");
+    element.classList.toggle("hidden");
+}
