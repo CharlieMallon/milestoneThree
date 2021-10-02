0224befba4eaa94209ef373fd8d11333f97faf2e
CharlieMallon <penguin47@hotmail.co.uk>
2021-10-02 21:13:00 +0100

    initial commit

---
+++ cypress/integration/account.spec.js
@@ -0,0 +1,10 @@
+describe('The Accounts Page', () => {
+    beforeEach(() => {
+        cy.login()
+    })
+
+    it('greets with Username', () => {
+        cy.get('.header').should('contain', 'Cypresstestuser')
+    })
+
+})
